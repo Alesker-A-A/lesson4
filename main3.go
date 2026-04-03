c0e7a1338ec38fc4a1d535558969fb90b4b4fb84
Alesker-A-A <skeroff1@gmail.com>
2026-04-03 19:45:47 +0300

    Исправление ошибок

--- main3.go
+++ main3.go
@@ -9,15 +9,15 @@ type Task struct{
 var tasks []Task
 
 func showMenu() {
-    fmt.Printf("~~~ To-Do Лист ~~~")
-    fmt.Printf("1. Показать все задачи")
-    fmt.Printf("2. Добавить задачу")
-    fmt.Printf("3. Пометить как 'Выполнена'")
-    fmt.Printf("4. Удалить задачу")
-    fmt.Printf("5. Выйти")
+    fmt.Println("~~~ To-Do Лист ~~~")
+    fmt.Println("1. Показать все задачи")
+    fmt.Println("2. Добавить задачу")
+    fmt.Println("3. Пометить как 'Выполнена'")
+    fmt.Println("4. Удалить задачу")
+    fmt.Println("5. Выйти")
 }
 
 func main() {
-    showMenu
+    showMenu()
 
 }
