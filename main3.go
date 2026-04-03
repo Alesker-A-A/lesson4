48bcfd201986fa024af4f127de2e1abda9a5a2e6
Alesker-A-A <skeroff1@gmail.com>
2026-04-03 19:43:55 +0300

    Добавляю меню с выбором

--- main3.go
+++ main3.go
@@ -1,13 +1,23 @@
 package main
 
-import fmt
+import "fmt"
 
 type Task struct{
     Title string
     Done bool
 }
 var tasks []Task
-func main() {
 
+func showMenu() {
+    fmt.Printf("~~~ To-Do Лист ~~~")
+    fmt.Printf("1. Показать все задачи")
+    fmt.Printf("2. Добавить задачу")
+    fmt.Printf("3. Пометить как 'Выполнена'")
+    fmt.Printf("4. Удалить задачу")
+    fmt.Printf("5. Выйти")
+}
+
+func main() {
+    showMenu
 
 }
