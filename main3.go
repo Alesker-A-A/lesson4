c2440708c38a13a4def109b28172d7d169ec4014
Alesker-A-A <skeroff1@gmail.com>
2026-04-05 10:37:02 +0300

    добавил ``

--- main3.go
+++ main3.go
@@ -12,7 +12,7 @@ func showMenu() {
     fmt.Println("~~~ To-Do Лист ~~~")
     fmt.Println("1. Показать все задачи")
     fmt.Println("2. Добавить задачу")
-    fmt.Println("3. Пометить как 'Выполнена'")
+    fmt.Println(`3. Пометить как "Выполнена"`)
     fmt.Println("4. Удалить задачу")
     fmt.Println("5. Выйти")
 }
