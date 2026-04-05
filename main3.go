65007da040fa277b98f4752d43d889b99c46d70e
unknown <skeroff1@gmail.com>
2026-04-05 11:32:41 +0300

    пробую

---
+++ main3.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Task struct {
+	Title string
+	Done  bool
+}
+
+var tasks []Task
+
+func showMenu() {
+	fmt.Println("~~~ To-Do Лист ~~~")
+	fmt.Println("1. Показать все задачи")
+	fmt.Println("2. Добавить задачу")
+	fmt.Println(`3. Пометить как "Выполнена"`)
+	fmt.Println("4. Удалить задачу")
+	fmt.Println("5. Выйти")
+}
+
+func main() {
+	showMenu()
+
+}
